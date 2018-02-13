999c14ff56b0f25f159f415f3f65e9fb99ec4511
dima <d.starenkiy@ukr.net>
2018-02-13 10:33:38 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,19 @@ var gulp = require('gulp'),
     cssnext = require('postcss-cssnext'),
     shortcss = require('postcss-short'),
     cssmin = require('gulp-minify-css'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    php = require('gulp-connect-php'),
+    browserSync = require('browser-sync');
     
+gulp.task('browser-sync', function() {
+        browserSync({
+            server: {
+                baseDir: './'
+            },
+            notify: false,
+            });
+});
+ 
 gulp.task('pug', function buildHTML() {
   var pug = require('gulp-pug');
   return gulp.src('*.pug')
@@ -35,12 +46,13 @@ gulp.task('autoprefixer', function () {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', ['sass', 'pug', 'autoprefixer']);
+gulp.task('default', ['sass', 'pug', 'autoprefixer', 'browser-sync']);
 
-gulp.task('watch', function() {
+gulp.task('watch', ['sass', 'pug', 'autoprefixer', 'browser-sync'], function() {
     gulp.watch('./sass/*.scss', ['sass']);
     gulp.watch('./css/*.css', ['sass']);
     gulp.watch('*.pug', ['pug']);
     gulp.watch('*.html', ['pug']);
     gulp.watch('./css/*.css', ['autoprefixer']);
+    gulp.watch('*.html', browserSync.reload);
 }); 
