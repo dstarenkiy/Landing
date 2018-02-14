081cf65cc441d93320088a05409e7bec5494bca8
dima <d.starenkiy@ukr.net>
2018-02-14 11:48:32 +0200

    new commitnew commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,48 @@
 'use strict';
 
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    sass = require('gulp-sass'),
-    pug = require('gulp-pug'),
-    postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer'),
-    cssnext = require('postcss-cssnext'),
-    shortcss = require('postcss-short'),
-    cssmin = require('gulp-minify-css'),
-    sourcemaps = require('gulp-sourcemaps'),
-    php = require('gulp-connect-php'),
-    browserSync = require('browser-sync');
-      
-    gulp.task('sass', function () {
-        return gulp.src('./sass/**/*.scss')
-          .pipe(sass().on('error', sass.logError))
-          .pipe(gulp.dest('./css'))
-    });
+    gp = require('gulp-load-plugins')(),   
+    browserSync = require('browser-sync').create();
 
-    gulp.task('pug', function () {
-        return gulp.src('./pug/*.pug')
-        .pipe(pug({pretty: true}))
-        .pipe(gulp.dest('./'))
-    });
-
-    gulp.task('autoprefixer', function () {
-        return gulp.src('./css/*.css')
-        .pipe(sourcemaps.init())       
-        .pipe(postcss([ autoprefixer({browsers: 'last 2 versions'}) ]))
-        .pipe(sourcemaps.write('.'))    
-        .pipe(gulp.dest('./css'));
-    });
-
-    gulp.task('browser-sync', function() {
-        browserSync({
+    gulp.task('serve', function() {
+        browserSync.init({
             server: {
-                baseDir: './'
-            },
-            notify: false,
+                baseDir: './build'
+            }            
         });
+        browserSync.watch('build', browserSync.reload)
     });
-     
-    gulp.task('build', function () {
-        gulp.watch('./sass/**/*.scss', gulp.series('sass'));
-        gulp.watch('./pug/*.pug', gulp.series('pug'));
-        gulp.watch('./*.html').on('change', browserSync.reload);    
+    
+    gulp.task('pug', function () {
+        return gulp.src('src/*.pug')
+        .pipe(gp.pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('build'))
+        .on('end',browserSync.reload);
     });  
+
+    gulp.task('sass', function () {
+        return gulp.src('src/sass/*.scss')
+        .pipe(gp.sourcemaps.init())
+          .pipe(gp.sass({}))
+          .pipe(gp.autoprefixer({
+              browsers: ['last 10 versions']
+          }))
+          .pipe(gp.cssbeautify())
+          .pipe(gp.sourcemaps.write())
+          .pipe(gulp.dest('build/css/'))
+          .pipe(browserSync.reload({
+              stream: true
+          }));
+    });    
      
-    gulp.task('watch', gulp.series('sass','pug',
-        gulp.parallel('build','browser-sync')
-    ));
+    gulp.task('watch', function () {
+        gulp.watch('src/sass/*.scss', gulp.series('sass'));
+        gulp.watch('src/*.pug', gulp.series('pug'));            
+    }); 
          
-    gulp.task('default', gulp.series('sass','pug',
-        gulp.parallel('watch','browser-sync')
+    gulp.task('default', gulp.series(
+        gulp.parallel('pug','sass'),
+        gulp.parallel('watch','serve')
     ));
