f77e9f525e9b052c66ec42ca4f2cef3db250fd6c
dima <d.starenkiy@ukr.net>
2018-02-15 09:41:42 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require('gulp'),
     gp = require('gulp-load-plugins')(),   
-    browserSync = require('browser-sync').create();
+    browserSync = require('browser-sync').create()
 
     gulp.task('serve', function() {
         browserSync.init({
