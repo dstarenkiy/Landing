443eff493165a7baefa698085e8ec1a79dd2bd19
dima <d.starenkiy@ukr.net>
2018-02-16 10:29:12 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,59 @@
 'use strict';
 
 var gulp = require('gulp'),
-    gp = require('gulp-load-plugins')(),   
+    gp = require('gulp-load-plugins')(),
+    mqpacker = require('css-mqpacker'),
     browserSync = require('browser-sync').create();
-
-    gulp.task('serve', function() {
-        browserSync.init({
-            server: {
-                baseDir: './build'
-            }           
-        });
-        browserSync.watch('build', browserSync.reload)
+   
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }           
     });
-    
-    gulp.task('pug', function () {
-        return gulp.src('src/*.pug')
-        .pipe(gp.pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('build'))
-        .on('end',browserSync.reload);
-    });  
+    browserSync.watch('build', browserSync.reload)
+});
 
-    gulp.task('sass', function () {
-        return gulp.src('src/sass/*.scss')
-        .pipe(gp.sourcemaps.init())
-          .pipe(gp.sass({}))
-          .pipe(gp.autoprefixer({
-              browsers: ['last 10 versions']
-          }))
-          .pipe(gp.cssbeautify())
-          .pipe(gp.sourcemaps.write())
-          .pipe(gulp.dest('build/css/'))
-          .pipe(browserSync.reload({
-              stream: true
-          }));
-    });    
-     
-    gulp.task('watch', function () {
-        gulp.watch('src/sass/*.scss', gulp.series('sass'));
-        gulp.watch('src/*.pug', gulp.series('pug'));            
-    }); 
-         
-    gulp.task('default', gulp.series(
-        gulp.parallel('pug','sass'),
-        gulp.parallel('watch','serve')
-    ));
+gulp.task('pug', function () {
+    return gulp.src('src/*.pug')
+    .pipe(gp.pug({
+        pretty: true
+    }))
+    .pipe(gulp.dest('build'))
+    .on('end',browserSync.reload);
+});  
+
+gulp.task('sass', function () {
+    var processors = ([
+        mqpacker({
+          sort: true
+        })
+      ]);
+    return gulp.src('src/sass/*.scss')
+    .pipe(gp.sourcemaps.init())
+        .pipe(gp.sass({}))
+        .pipe(gp.postcss(processors))      
+        .pipe(gp.autoprefixer({
+            browsers: ['last 2 versions', 'ie > 10']
+        }))        
+        .pipe(gp.sourcemaps.write())        
+        .pipe(gp.csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        }))
+        .pipe(gulp.dest('build/css/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});    
+    
+gulp.task('watch', function () {
+    gulp.watch('src/sass/*.scss', gulp.series('sass'));
+    gulp.watch('src/*.pug', gulp.series('pug'));            
+}); 
+        
+gulp.task('default', gulp.series(
+    gulp.parallel('pug','sass'),
+    gulp.parallel('watch','serve')
+));
